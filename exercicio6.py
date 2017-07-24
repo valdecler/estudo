8c054424b66fe200d883bde31add39a7b8c9d33e
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-24 12:33:14 -0300

    Add files via upload

---
+++ exercicio6.py
@@ -0,0 +1,14 @@
+
+idade = 25
+menssagem = "parabens pelo seus " + idade+ " anos rd aniversario"
+
+print (menssagem) 
+  # o erro neste exeplo ocorre pq a variavel (idade) nao foi especificada entao 
+  #python retorna um erro de strings para corrigir esse erro de ve ser inserido 
+  #str(variavel) como no exeplo a baixo, porem o codigo a baixo nao ser execultado
+  #se o codigo acima nao for corrigido ou excluido  
+
+idade = 25
+menssagem = "parabens pelo seus " + str(idade) + " anos feliz aniversario"
+
+print (menssagem) 
