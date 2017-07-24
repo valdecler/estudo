5aff5ab80fa014f9a31d09abf423e159cf409b13
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-24 10:57:32 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,17 @@
+
+menssagem_1 = ' em outro mundo outro lugar'
+
+print (menssagem_1)
+
+  # Neste primeiro exeplo de erro o problema esta no print onde foi aberto 
+  #parentesses porem nao foi fechado impossibilitando o python de ler o codigo 
+  
+
+menssagem_2 = " so mais uma fase a superar
+
+print (menssagem_2)
+
+  # Neste exemplo o erro esta no valor da variavel foi aberto "" porem 
+  #nao foi fechada nao atribuito valor a variavel menssagem_2
+  
+
