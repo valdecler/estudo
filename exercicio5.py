199c24508b290ded733e2205a5291ae75f88a37d
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-25 14:37:43 -0300

    Add files via upload

---
+++ exercicio5.py
@@ -0,0 +1,7 @@
+
+print (10+2*5)
+
+print ((10+2)*5)
+
+  # O resultado e diferente por que apos inserir parênteses na operacao o python
+  #eu estou especificando a ordem em que deve ser execultado o programa 
