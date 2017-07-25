6357cb6de6923b1bae6eff4bc27d75e1ce310da0
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-25 15:56:48 -0300

    Add files via upload

---
+++ exercicio10.py
@@ -0,0 +1,8 @@
+  #lista
+  
+nome = ['galinha','rato','gato','lobo','outro',' novo']
+
+print (nome [-1])
+
+ # isso ocoore por que estamos solicitando a umltima palavra da lista indica -1
+ # que sempre vai intendender como ultima coisa entre aspas numa corada 
