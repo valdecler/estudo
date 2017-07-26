ffbf0e76f8f1da7349acb39c56be2573fbd3b2a0
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-26 19:47:15 -0300

    Add files via upload

---
+++ exercicio18.py
@@ -0,0 +1,6 @@
+
+# Faça a mesma coisa do desafio 17: só que passando a posição que vc deseja.
+
+carro = [ ' monza ' , ' celta ' , ' palio ' , ' parati ' , ' vectra ' , ' corsa ' ]
+first_owned = carro.pop(4)
+print ( " Eu tenho um "  + first_owned.title () +  " mas bati ele no poste kkk " )
