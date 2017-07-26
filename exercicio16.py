c488ee7a777cc5146d5de32d4e18af03a95cc8b5
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-26 19:18:38 -0300

    Add files via upload

---
+++ exercicio16.py
@@ -0,0 +1,15 @@
+
+ # lista 
+ # Crie um script que remova o ultimo elemento da lista de forma que 
+ #vc ainda possa usar o elemento removido.  explique  o método.
+
+animal = ['gamba','coelho','passaro','jacare','cobra']
+print (animal)
+
+popped_animal = animal.pop()
+print (animal)
+
+print (popped_animal)
+
+#O resultado mostra que o valor 'cobra' foi removido a partir da
+#extremidade da lista e está armazenado na variável popped_animal
