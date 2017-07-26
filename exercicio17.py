17c1ccd480f1d1fc1bd49e593082aa11c5362ef2
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-26 19:37:18 -0300

    Add files via upload

---
+++ exercicio17.py
@@ -0,0 +1,9 @@
+
+ 
+# Crie um script que  remova o ultimo elemento da lista com método pop(), 
+#armazene em uma segunda variável, e faça a concatenação dele com uma 
+#frase “string” que comece com a primeira letra maiúscula
+
+carro = ['monza','celta','palio','parati','vectra','corsa']
+last_ownet = carro.pop ()
+print ("Eu tenho um " + last_ownet.title () + " mas bati ele no poste kkk" )
