871c13ea5a182d779a817e9bbd8b56c27196cf22
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-27 18:21:22 -0300

    Add files via upload

---
+++ exercicio22.py
@@ -0,0 +1,9 @@
+
+ #:Crie um script que que altere a ordem da lista , para que os elementos  
+#fiquem de  forma alfabética .
+ 
+nome = ['bruno','amanda','daniele','carlos','fabio','emilio',]
+print (nome)
+
+nome.sort()
+print (nome)
