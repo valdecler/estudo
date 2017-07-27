8bd0e151ed262e04e658b9bdd7972c394b93ce5b
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-27 19:22:16 -0300

    Add files via upload

---
+++ exercicio24.py
@@ -0,0 +1,10 @@
+
+ #Crie um script que altere a ordem da lista de forma alfabética, mais 
+ #que não altere o valor real
+ 
+nome = ['bruno','amanda','daniele','carlos','fabio','emilio',]
+print (nome)
+
+print(sorted(nome))
+
+print (nome) 
