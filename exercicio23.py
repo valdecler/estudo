07417735265989b1a88ce16e13d6b47bdefd9a56
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-27 19:10:37 -0300

    Add files via upload

---
+++ exercicio23.py
@@ -0,0 +1,8 @@
+
+ #Crie um script que altere a ordem da lista, de forma alfabética 
+ #inversa, com o parâmetro True
+
+nome = ['bruno','amanda','daniele','carlos','fabio','emilio',]
+print (nome)
+nome.sort(reverse=True)
+print (nome)
