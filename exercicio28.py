3d7efb39b0f0617fba5888f55f045dbebf1730a7
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-28 13:28:24 -0300

    Add files via upload

---
+++ exercicio28.py
@@ -0,0 +1,10 @@
+
+ #Crie um script que tente acessar uma posição em sua lista, maior que 
+ #os elementos que nela possui, vai ocorrer um erro, explique pq isso acontece
+
+nome = ['janaina','camila','renata','ana','bruna','daiane']
+print (nome[6])
+
+ # esse erro ocorre pq o apos o python ler o codigo vai indentificar que o intem
+ #solicitado nao exixte e vai retorna um erro o certo caso nao saiba qual o ultimo
+ #item da lista deve se usar [-1] assim podemos efitar erros 
