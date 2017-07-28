daafa47c70e350fbbd5108583e6fd68f44794dff
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-28 12:10:51 -0300

    Add files via upload

---
+++ exercicio26.py
@@ -0,0 +1,10 @@
+
+# Crie um script que mostre o comprimento de sua lista, com o número de 
+#elementos contidos nela
+
+nome = ['janaina','camila','renata','ana','bruna','daiane'] 
+print (nome)
+
+print (len(nome))
+
+
