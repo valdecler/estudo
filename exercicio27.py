cad2a00e347bda81736f695d501b42ace21ba37a
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-28 13:29:07 -0300

    Add files via upload

---
+++ exercicio27.py
@@ -0,0 +1,11 @@
+
+ #Crie um script que mostre o comprimento de um elemento de sua lista, 
+ #quantos “caracteres” ele possui
+
+nome = ['janaina','camila','renata','ana','bruna','daiane'] 
+print (nome)
+
+print (len(nome))
+
+print (nome[1])
+print (len(nome [1]))
