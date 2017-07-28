e45d78b5735cd8e918ba287437703606d6abc26b
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-28 11:56:25 -0300

    Add files via upload

---
+++ exercicio25.py
@@ -0,0 +1,12 @@
+
+ #Crie um script que inverte a ordem da lista, mais que vc possa reverter 
+ #para a forma anterior
+
+nome = ['janaina','camila','renata','ana','bruna','daiane'] 
+print (nome)
+
+nome.reverse()
+print (nome)
+
+nome.reverse()
+print (nome)
