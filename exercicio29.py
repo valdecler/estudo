247a90704eb86829b4a09f21862c30b288d0a0f0
valdecler <30377147+valdecler@users.noreply.github.com>
2017-07-28 22:15:14 -0300

    Add files via upload

---
+++ exercicio29.py
@@ -0,0 +1,28 @@
+
+pers = ['comprador_1','vendendor','comprador_2','espaco',]
+
+carros = ['palio','uno','celta','corsa','meriva','jeta']
+
+print ('\n\t' + pers[1] + ' Seja bem vindo! senhor ' + pers[0] ) 
+
+print ('\n\t' + pers[0] + ' quantos caros vc tem a venda?' )
+
+print ('\n\t' + pers[1] +  ' hoje nos temos disponivel senhor, sao ') 
+
+print (len (carros))
+
+print ('\n\t' + pers[0] + 'Quais sao os carros')
+
+print ('\n\t' + pers[1] + ' Os seguintes carros')
+
+print (carros[0].title())
+print (carros[1].title())
+print (carros[2].title())
+print (carros[3].title())
+print (carros[4].title())
+print (carros[5].title())
+
+print ('\n\t' + pers[0] + ' Gotei muito do ' + carros[3].title() + ' quanto custa?')
+
+print ('\n\t' + pers[1] + ' Senho ' + pers[0] + ' o ' + carros[3].title() + ' custa 10 mil rsrsrsrsrs esta barato')
+
